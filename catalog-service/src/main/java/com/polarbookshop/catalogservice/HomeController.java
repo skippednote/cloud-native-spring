31a32f30cfe23cffa1b4b25f2c7a8e1992b352de
Bassam Ismail <skippednote@gmail.com>
2022-02-20 09:46:05 +0530

    ch-02

---
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the book catalog!";
+    }
+}
